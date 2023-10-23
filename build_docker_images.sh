edbcd5cb425d1c23f7dee06936524bdf7e4a68a7
esgn <5435148+esgn@users.noreply.github.com>
2023-10-23 10:42:47 +0200

    bash script documentation

--- build_docker_images.sh
+++ build_docker_images.sh
@@ -1,7 +1,22 @@
+#!/usr/bin/env bash
+
+# In case the repository was not cloned with --recurse-submodules
 git submodule update --init --recursive
+
+# Define number of cores for compilation
 CORES=$(($(nproc)-2))
+
+# Build base image
 docker build -f base.dockerfile --no-cache --build-arg JOBS=$CORES -t ignfab/geoflow-bundle-base:latest .
+
+# Build builder image based on base image
 docker build -f builder.dockerfile --no-cache --build-arg JOBS=$CORES -t ignfab/geoflow-bundle-builder:latest . 
+
+# Create reconstruct image with single workflow
 docker build -f lod22-reconstruct.dockerfile --no-cache -t ignfab/lod22-reconstruct:latest .
+
+# Create reconstruct image with batch workflow
 docker build -f lod22-reconstruct-batch.dockerfile --no-cache -t ignfab/lod22-reconstruct-batch:latest .
+
+# Create reconstruct image with stream workflow
 docker build -f lod22-reconstruct-stream.dockerfile --no-cache -t ignfab/lod22-reconstruct-stream:latest .
